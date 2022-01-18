dca391c811fd22398855c6b13b33d5919adcf668
user <slavad878@gmail.com>
2022-01-18 04:55:25 +0300

    select a specific passenger who flight 2 days ago

--- interval.sql
+++ interval.sql
@@ -1,3 +1,14 @@
-SELECT f.flight_no FROM flights f
+SELECT
+  t.passenger_name,
+  b.book_date
+FROM flights f
+  JOIN boarding_passes bp
+    ON f.flight_id = bp.flight_id
+  JOIN tickets t
+    ON t.ticket_no = bp.ticket_no
+  JOIN bookings b
+    ON b.book_ref = t.book_ref
 WHERE f.scheduled_departure::date = bookings.now()::date - INTERVAL '2 day'
-LIMIT 5;
+AND f.departure_airport = 'SVO'
+AND f.arrival_airport = 'OVB'
+AND bp.seat_no = '1A';

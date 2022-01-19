ec1dd516169324817463fdcf43eeb070ee23b2d6
user <slavad878@gmail.com>
2022-01-19 05:03:51 +0300

    distinct empty rows

--- not-exists.sql
+++ not-exists.sql
@@ -1,3 +1,11 @@
-SELECT * FROM flights f
+SELECT count(*) FROM flights f
+  JOIN seats s
+  ON f.aircraft_code = s.aircraft_code
 WHERE f.flight_no = 'PG0404'
-AND f.scheduled_departure::date = bookings.now()::date - INTERVAL '1 day';
+AND f.scheduled_departure::date = bookings.now()::date - INTERVAL '1 day'
+AND NOT EXISTS (
+  SELECT NULL
+  FROM boarding_passes bp
+  WHERE f.flight_id = bp.flight_id
+  AND s.seat_no = bp.seat_no
+);

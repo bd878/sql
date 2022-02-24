7e08725d23011cde1ffaf34b7e71bcbd0a74323d
user <slavad878@gmail.com>
2022-02-24 09:26:54 +0300

    select sum in day [fail still]

--- sandbox.sql
+++ sandbox.sql
@@ -1,13 +1,8 @@
 SELECT
-  a.model,
-  sum(f.actual_arrival - f.actual_departure) OVER (
-    PARTITION BY date_trunc('month', f.actual_departure)
-    ORDER BY f.actual_departure
-  ) AS flight_hours
+  count(*)
 FROM
-  flights f
-  JOIN aircrafts a
-    ON f.aircraft_code = a.aircraft_code
-  WHERE f.actual_arrival IS NOT NULL
-    AND f.actual_departure IS NOT NULL
-  ORDER BY 1;
+  flights
+WHERE aircraft_code = 'SU9'
+  AND actual_departure IS NOT NULL
+  AND actual_arrival IS NOT NULL
+  AND actual_departure::date = '2017-07-16'::date;

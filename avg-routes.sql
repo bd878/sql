7479840cf259fa3687bb1a4de7adbf6dd565deab
user <slavad878@gmail.com>
2022-03-06 06:30:00 +0300

    trying recursive ranges

--- avg-routes.sql
+++ avg-routes.sql
@@ -1,11 +1,19 @@
 SELECT
-  r.aircraft_code AS a_code,
+  a.aircraft_code,
   a.model,
-  r.aircraft_code AS r_code,
-  count(r.aircraft_code) AS num_routes,
-  count(r.aircraft_code) / () AS fraction
+  r.aircraft_count,
+  round( r.aircraft_count::numeric /
+         r.total_count::numeric, 3 ) AS fraction
 FROM aircrafts a
-LEFT OUTER JOIN routes r
-  ON r.aircraft_code = a.aircraft_code
-GROUP BY 1, 2, 3
-ORDER BY 4 DESC;
+LEFT OUTER JOIN (
+  SELECT
+    r1.aircraft_code,
+    count(r1.aircraft_code) AS aircraft_count,
+    ( SELECT count(r2.flight_no)
+      FROM routes r2
+    ) AS total_count
+  FROM routes r1
+  GROUP BY r1.aircraft_code
+) r
+ON r.aircraft_code = a.aircraft_code
+ORDER BY r.aircraft_count ASC;
--- sandbox.sql
+++ sandbox.sql
@@ -1,8 +1,13 @@
-SELECT
-  r1.aircraft_code,
-  count(r1.aircraft_code) AS aircraft_count,
-  ( SELECT count(r2.flight_no)
-    FROM routes r2
-  ) AS total_count
-FROM routes r1
-GROUP BY r1.aircraft_code;
+WITH RECURSIVE ranges( n, min_sum, max_sum ) AS (
+  VALUES ( 1, 0, 100000 ),
+         ( 1, 100000, 200000 ),
+         ( 1, 200000, 300000 )
+  UNION
+  SELECT n + 1, min_sum + 100000, max_sum + 100000
+    FROM ranges
+    WHERE max_sum < 
+      ( SELECT max(total_amount) FROM bookings )
+    AND n < 100
+)
+SELECT * FROM ranges
+LIMIT 100;

a99639ff0be833e3e5ad89b93b6d727add9a9f29
user <slavad878@gmail.com>
2022-03-11 08:32:57 +0300

    consider fare conditions in flights fractions

--- flights-seats-count.sql
+++ flights-seats-count.sql
@@ -4,13 +4,15 @@ SELECT
   f.departure_city,
   f.arrival_city,
   f.aircraft_code,
+  tf.fare_conditions,
   count(tf.ticket_no) AS fact_passengers,
   ( SELECT count(s.seat_no)
       FROM seats s
       WHERE s.aircraft_code = f.aircraft_code
+        AND s.fare_conditions = tf.fare_conditions
   ) AS total_seats
 FROM flights_v f
-JOIN ticket_flights tf
-ON f.flight_id = tf.flight_id
+  JOIN ticket_flights tf
+  ON f.flight_id = tf.flight_id
 WHERE f.status = 'Arrived'
-GROUP BY 1, 2, 3, 4, 5;
+GROUP BY 1, 2, 3, 4, 5, 6;
--- flights-seats-fraction.sql
+++ flights-seats-fraction.sql
@@ -5,24 +5,27 @@ WITH tickets_seats AS
       f.departure_city,
       f.arrival_city,
       f.aircraft_code,
+      tf.fare_conditions,
       count(tf.ticket_no) AS fact_passengers,
       ( SELECT count(s.seat_no)
           FROM seats s
           WHERE s.aircraft_code = f.aircraft_code
+            AND s.fare_conditions = tf.fare_conditions
       ) AS total_seats
     FROM flights_v f
-    JOIN ticket_flights tf
-    ON f.flight_id = tf.flight_id
+      JOIN ticket_flights tf
+      ON f.flight_id = tf.flight_id
     WHERE f.status = 'Arrived'
-    GROUP BY 1, 2, 3, 4, 5
+    GROUP BY 1, 2, 3, 4, 5, 6
   )
 SELECT
   ts.departure_city,
   ts.arrival_city,
+  ts.fare_conditions,
   sum(ts.fact_passengers) AS sum_fact_passengers,
   sum(ts.total_seats) AS sum_total_seats,
   round( sum(ts.fact_passengers)::numeric /
          sum(ts.total_seats)::numeric, 2 ) AS fraction
 FROM tickets_seats ts
-GROUP BY ts.departure_city, ts.arrival_city
+GROUP BY ts.departure_city, ts.arrival_city, ts.fare_conditions
 ORDER BY ts.departure_city;

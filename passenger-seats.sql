d25859356b9930c70e83f8f4098c1fb7cbc05db7
user <slavad878@gmail.com>
2022-03-13 10:31:06 +0300

    passenger seats with fare conditions (nested joins)

--- passenger-seats.sql
+++ passenger-seats.sql
@@ -1,6 +1,11 @@
-SELECT bp.seat_no, t.passenger_name
-FROM boarding_passes bp
-  JOIN tickets t
-  ON bp.ticket_no = t.ticket_no
+SELECT tf.fare_conditions, bp.seat_no, t.passenger_name
+  FROM (
+    ticket_flights tf
+    JOIN tickets t
+      ON t.ticket_no = tf.ticket_no
+  )
+  JOIN boarding_passes bp
+    ON bp.flight_id = tf.flight_id
+    AND tf.ticket_no = bp.ticket_no
 WHERE bp.flight_id = 27584
 ORDER BY t.passenger_name DESC;

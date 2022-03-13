91425b4c257d98702a8da8656a76071b3d2b9c84
user <slavad878@gmail.com>
2022-03-13 11:20:17 +0300

    left outer join passengers with seats

--- passenger-seats.sql
+++ passenger-seats.sql
@@ -1,11 +1,26 @@
-SELECT tf.fare_conditions, bp.seat_no, t.passenger_name
-  FROM (
-    ticket_flights tf
-    JOIN tickets t
-      ON t.ticket_no = tf.ticket_no
-  )
-  JOIN boarding_passes bp
-    ON bp.flight_id = tf.flight_id
-    AND tf.ticket_no = bp.ticket_no
-WHERE bp.flight_id = 27584
-ORDER BY t.passenger_name DESC;
+SELECT
+  s.seat_no,
+  substr( b.passenger_name,
+    strpos( b.passenger_name, ' ' ) + 1
+  ) AS lastname,
+  left( b.passenger_name,
+    strpos( b.passenger_name, ' ' ) - 1
+  ) AS firstname
+FROM seats s
+  LEFT OUTER JOIN (
+    SELECT
+      tf.fare_conditions,
+      t.passenger_name,
+      bp.seat_no
+    FROM (
+      ticket_flights tf
+      JOIN tickets t
+        ON t.ticket_no = tf.ticket_no
+    )
+      JOIN boarding_passes bp
+        ON bp.flight_id = tf.flight_id
+        AND tf.ticket_no = bp.ticket_no
+    WHERE bp.flight_id = 27584
+  ) b
+  ON s.seat_no = b.seat_no
+WHERE s.aircraft_code = 'SU9'
